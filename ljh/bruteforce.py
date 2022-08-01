7e1bdd73ebf204ae3f356f2c1782baa3f86afe1d
bebusl <bebus1998@naver.com>
2022-08-02 05:44:32 +0900

    ljh: 폴더 구조 변경

---
+++ ljh/bruteforce.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+nums = [2,7,11,15]
+target = 9 
+
+#index를 하면 안돼
+
+start_idx = 0
+
+for(idx =0 ;idx<nums.length; i++):
+    nums.pop(0)
+    num[0]+num[1]
+    start_idx+=1
