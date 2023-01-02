5a4928c429f17366a0eafd3794d15bbacfdf9678
이진희 <49019236+bebusl@users.noreply.github.com>
2023-01-02 09:30:56 +0900

    ljh: 소수 판별(에라토스테네스의 체가 적합하지 않은 경우)
    * 해당 문제에서는 에라토스테네스의 체가 적합하지 않았음.
    * 1~n사이의 소수를 모두 구해라 => 에라토스테네스의 체가 적합
    * 얘가 소수인지 판별하라 => 그냥 1~제곱근(n)범위에서 나눠떨어지는 수가 하나라도 있으면 소수가 아니므로 바로 반환해주는 게 훨씬 빠르므로 에라토스테네스의 체가 적합하지 않음

--- ljh/programmers_lv2/92335_2022kakao_prime_number.py
+++ ljh/programmers_lv2/92335_2022kakao_prime_number.py
@@ -1,43 +1,26 @@
+import math
 def translate(n, number):
     n_ory = ''
-    while (number > 0):
-        n_ory += '%d' % (number % n)
+    while (number >= 1):
+        n_ory += str(number%n)
         number //= n
     return n_ory[::-1]
 
-
-def primeArray(max_value):
-    uclid = [False, False]
-    uclid.extend([True for i in range(max_value-1)])
-    idx = 2
-    while (True):
-        if (not uclid[idx]):
-            idx += 1
-            continue
-        if (idx > max_value**0.5):
-            break
-        tmp_idx = 2
-        while (True):
-            if (idx * tmp_idx > max_value):
-                break
-            uclid[idx * tmp_idx] = False
-            tmp_idx+=1
-        idx += 1
-    return uclid
-
+def isPrime(value):
+    if(value ==1 ):
+        return False
+    for i in range(2, math.floor(value**0.5)+1):
+        if(value % i == 0):
+            return False
+    return True
 
 def solution(n, k):
     answer = 0
     n_ory = translate(k,n)
     number_set = list(map(int,filter(None,n_ory.split('0'))))
 
-    max_num =max(number_set)
-    uclid = primeArray(max_num)
-
     for num in number_set:
-        if(uclid[num]):
+        if(isPrime(num)):
             answer+=1
 
-    return answer if answer else -1
-
-print(solution(110011,10))
+    return answer
